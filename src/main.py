601e4fe356878c421a7cedecee357dc4c713ebf2
Jared Ramsey <ramsey_jared@bah.com>
2021-09-12 14:25:18 -0400

    Adding movement of character

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import tcod
+from tcod.event import Event
 
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
@@ -13,6 +16,8 @@ def main() -> None:
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
+    event_handler = EventHandler()
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
@@ -23,11 +28,22 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
             root_console.print(x=player_x, y=player_y, string="@")
-            
+
             context.present(root_console)
 
+            root_console.clear()
+
             for event in tcod.event.wait():
-                if event.type == "QUIT":
+                action = event_handler.dispatch(event)
+                
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                if isinstance(action, EscapeAction):
                     raise SystemExit()
 
 
