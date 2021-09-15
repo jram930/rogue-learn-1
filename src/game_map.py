da4223730cd7a31bffc4d089e52fc48e685ee815
Jared Ramsey <ramsey_jared@bah.com>
2021-09-14 20:49:47 -0400

    Adding basic FOV handling

--- src/game_map.py
+++ src/game_map.py
@@ -17,4 +17,15 @@ class GameMap:
 		return 0 <= x < self.width and 0 <= y < self.height
 
 	def render(self, console: Console) -> None:
-		console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+		"""
+		Renders the map.
+ 
+		If a tile is in the "visible" array, then draw it with the "light" colors.
+		If it isn't, but it's in the "explored" array, then draw it with the "dark" colors.
+		Otherwise, the default is "SHROUD".
+		"""
+		console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+			condlist=[self.visible, self.explored],
+			choicelist=[self.tiles["light"], self.tiles["dark"]],
+			default=tile_types.SHROUD
+		)
--- src/main.py
+++ src/main.py
@@ -49,7 +49,7 @@ def main() -> None:
 		screen_width,
 		screen_height,
 		tileset=tileset,
-		title="Yet Another Roguelike Tutorial",
+		title="rogue-learn-1",
 		vsync=True,
 	) as context:
 		root_console = tcod.Console(screen_width, screen_height, order="F")
